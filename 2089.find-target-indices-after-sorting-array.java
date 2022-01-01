7293acdc5cc24ada673fc5daf8974cc08dfb6a9e
Vipul <pawanvishwakarma521@gmail.com>
2022-01-01 23:24:29 +0530

    2089

--- 2089.find-target-indices-after-sorting-array.java
+++ 2089.find-target-indices-after-sorting-array.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
  * @lc app=leetcode id=2089 lang=java
  *
@@ -7,8 +11,15 @@
 // @lc code=start
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        
+        Arrays.sort(nums);
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                ans.add(i);
+        }
+        return ans;
+
     }
 }
 // @lc code=end
-
