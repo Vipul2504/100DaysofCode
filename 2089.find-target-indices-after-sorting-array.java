3226cda2d463a71b232b18d7201a6644e4a2ef72
Vipul <pawanvishwakarma521@gmail.com>
2022-01-01 23:01:03 +0530

    first commit

---
+++ 2089.find-target-indices-after-sorting-array.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2089 lang=java
+ *
+ * [2089] Find Target Indices After Sorting Array
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        
+    }
+}
+// @lc code=end
+
