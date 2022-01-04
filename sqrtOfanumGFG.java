47a613f4fed0d22fb641736eb835b8c9de937a1a
Vipul <pawanvishwakarma521@gmail.com>
2022-01-04 21:34:27 +0530

    sqrtofnumGFG

---
+++ sqrtOfanumGFG.java
@@ -0,0 +1,23 @@
+public class sqrtOfanumGFG {
+    public static void main(String[] args) {
+        int x = 11;
+        System.out.print(floorSqrt(x));
+    }
+
+    static int floorSqrt(int x) {
+        // Base cases
+        if (x == 0 || x == 1)
+            return x;
+
+        // Starting from 1, try all numbers until
+        // i*i is greater than or equal to x.
+        int i = 1, result = 1;
+
+        while (result <= x) {
+            i++;
+            result = i * i;
+        }
+        return i - 1;
+    }
+
+}
